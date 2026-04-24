3a536f3dced90b628b0600cfab486c60d22bc987
cloudssssssssss <staslubcenko@gmail.com>
2026-04-24 08:06:28 +0200

    65.1

---
+++ Task-65/src/app.js
@@ -0,0 +1,24 @@
+﻿import express from "express";
+import connectDB from "./config/db.js";
+import User from "./models/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("<h1>MongoDB Atlas is Live!</h1><a href='/users-db'>Fetch Users from Cloud</a>");
+});
+
+app.get("/users-db", async (req, res) => {
+    try {
+        const users = await User.find();
+        if (users.length === 0) return res.send("Connected! But database is empty. Go to Atlas and Insert a Document.");
+        res.json(users);
+    } catch (err) {
+        res.status(500).send("Error fetching data");
+    }
+});
+
+app.listen(3000, () => console.log("Server: http://localhost:3000"));
---
+++ Task-65/src/config/db.js
@@ -0,0 +1,14 @@
+﻿import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("🐘 MongoDB Atlas Connected (Banana-free zone)...");
+    } catch (err) {
+        console.error("Database connection error:", err.message);
+        process.exit(1);
+    }
+};
+export default connectDB;
